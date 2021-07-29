8794f603bd57175604586678910a8a25c099df26
isabellavjs <isabellavjs@gmail.com>
2021-07-29 18:58:24 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as description_file:
+    readme = description_file.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = [line for line in requirements_file]
+
+setup(
+    name="documentation",
+    version="0.1.0",
+    author="data_engineering",
+    python_requires=">=3.8.2",
+    description="Stores Data Engineering frameworks documentation",
+    long_description=readme,
+    url="https://github.com/betrybe/docs_framework",
+    install_requires=requirements,
+    packages=find_packages()
+)
