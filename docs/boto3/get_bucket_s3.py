d3b943d0b253d276d21a41da43df133a6134658d
isabellavjs <isabellavjs@gmail.com>
2021-07-30 17:18:56 -0300

    Add folders S3 and create routes

--- docs/boto3/get_bucket_s3.py
+++ docs/boto3/get_bucket_s3.py
@@ -1,8 +1,8 @@
 import boto3
 import os
 
-# assumes credentials & configuration are handled outside python in .aws directory or environment variables
-s3 = boto3.resource('s3')
+
+s3 = boto3.resource("s3")
 
 
 def download_s3_folder(bucket_name, s3_folder, local_dir=None):
@@ -15,16 +15,19 @@ def download_s3_folder(bucket_name, s3_folder, local_dir=None):
     """
     bucket = s3.Bucket(bucket_name)
     for obj in bucket.objects.filter(Prefix=s3_folder):
-        target = obj.key if local_dir is None \
+        target = (
+            obj.key
+            if local_dir is None
             else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        )
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
-        if obj.key[-1] == '/':
+        if obj.key[-1] == "/":
             continue
         bucket.download_file(obj.key, target)
 
 
-download_s3_folder("dataops-docs-trybe-us-east-2-679152", "saga/")
+# download_s3_folder("dataops-docs-trybe-us-east-2-679152", "saga/")
 
 
 download_s3_folder("dataops-docs-trybe-us-east-2-679152", "aiolia/")
